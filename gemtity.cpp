769da5cb1ec4a90fa6393d922eb9f01d36143097
Vincent Gefflaut <vincent.gefflaut@gmail.com>
2023-04-24 09:20:24 +0200

    Réarrangement des fichiers

---
+++ gemtity.cpp
@@ -0,0 +1,25 @@
+#include "gemtity.h"
+#include <Imagine/Graphics.h>
+using namespace Imagine;
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include<iostream>
+#include <string>
+using namespace std;
+
+// Méthodes de la classe GEMtity
+bool GEMtity::collision(GEMtity *entity){
+
+}
+void GEMtity::setImage(string chemin[]){
+
+}
+void GEMtity::setBehaviour(vector<double> pos, vector<double> speed, vector<double> acc){
+
+}
+
+// Constructeur de la classe GEMtity
+GEMtity::GEMtity(string chemin_image, vector<double> pos, vector<double> speed, vector<double> acc){
+
+}
