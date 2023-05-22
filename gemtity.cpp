8def080b5402bf144c062f492588a6bf645c1a89
Alexandre Lamotte <alexandre.lamotte@eleves.enpc.fr>
2023-05-22 09:08:26 +0200

    hitbox et comment

--- gemtity.cpp
+++ gemtity.cpp
@@ -1,4 +1,4 @@
-#include "gemtity.h"
+#include "GEMtity.h"
 #include <Imagine/Graphics.h>
 #include <Imagine/Images.h>
 using namespace Imagine;
@@ -14,7 +14,7 @@ using namespace std;
 using namespace std::chrono;
 
 // Méthodes de la classe GEMtity
-bool GEMtity::collision(GEMtity* entity){
+bool GEMtity::collision(GEMtity* entity){ //Fonction de détection de collision entre 2 entités
     for (int i = 0; i <= hitbox.size(); i++){
         QPointF point = hitbox.at(i);
         // On va maintenant vérifier si ce point est dans la hitbox de l'entité en argument
@@ -25,28 +25,28 @@ bool GEMtity::collision(GEMtity* entity){
         }
     }
 
-void GEMtity::setImage(string chemin){
+void GEMtity::setImage(string chemin){//Change l'image associée à une entité
     load(image,chemin);
 }
 void GEMtity::setBehaviour(double new_pos[2],  double new_speed[2], double new_acc[2]){
-    pos[0] = new_pos[0];
+    pos[0] = new_pos[0];//Change la position d'une entité
     pos[1] = new_pos[1];
-    speed[0] = new_speed[0];
+    speed[0] = new_speed[0];//Sa vitesse
     speed[1] = new_speed[1];
-    acc[0] = new_acc[0];
+    acc[0] = new_acc[0];//Son accélération
     acc[1] = new_acc[1];
-    for (int i = 0; i<hitbox.size(); i++ ){
+    for (int i = 0; i<hitbox.size(); i++ ){//Et déplace sa hitbox
         hitbox[i] = hitbox[i] + QPointF(new_pos[0]-pos[0], new_pos[1]-pos[1]);
     }
 }
 
-void GEMtity::setPos(double new_pos[2]){
+void GEMtity::setPos(double new_pos[2]){//Change uniquement la position
     setBehaviour(new_pos, speed, acc);
 }
-void GEMtity::setSpeed(double new_speed[2]){
+void GEMtity::setSpeed(double new_speed[2]){//Change uniquement la vitesse
     setBehaviour(pos, new_speed, acc);
 }
-void GEMtity::setAcc(double new_acc[2]){
+void GEMtity::setAcc(double new_acc[2]){//Change uniquement l'accélération
     setBehaviour(pos, speed, new_acc);
 }
 // Constructeur de la classe GEMtity
@@ -70,16 +70,20 @@ GEMtity::GEMtity(string path, double new_pos[2], double new_speed[2], double new
         }
     }
 }
-void GEMtity::display(){
+void GEMtity::display(){//Affichage d'une entité
     Imagine::display(image,pos[0],pos[1]);
 }
 
-void GEMtity::refresh(double time_now){
+void GEMtity::refresh(double time_now){//actualise les positions,vitesses et hitbox de l'entité
     double nouv_pos[2]={pos[0] + pow(time_now,2)/2*acc[0] + time_now*speed[0],
                         pos[1] + pow(time_now,2)/2*acc[1] + time_now*speed[1]};
+    double ancienne_pos[2] = {pos[0],
+                              pos[1]};
     setPos(nouv_pos);
     double nouv_speed[2] = {speed[0] + time_now*acc[0],
                            speed[1] + time_now*acc[1]};
     setSpeed(nouv_speed);
-
+    for (int i = 0; i<hitbox.size(); i++ ){
+        hitbox[i] = hitbox[i] + QPointF(pos[0]-ancienne_pos[0], pos[1]-ancienne_pos[1]);
+    }
 }
